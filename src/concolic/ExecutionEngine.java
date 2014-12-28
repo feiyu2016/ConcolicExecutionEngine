9c762dfdbc11f98ebaada060cbbe753ccbd52724
Wenhao Chen <xjtujack@gmail.com>
2014-12-28 14:56:56 -0600

    a

--- src/concolic/ExecutionEngine.java
+++ src/concolic/ExecutionEngine.java
@@ -1,10 +1,10 @@
 package concolic;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
 
 import staticFamily.StaticApp;
+import tools.Adb;
 import zhen.version1.UIModelGenerator;
 import zhen.version1.component.Event;
 import zhen.version1.component.UIModelGraph;
@@ -12,6 +12,7 @@ import zhen.version1.component.UIModelGraph;
 public class ExecutionEngine {
 
 	private StaticApp testApp = null;
+	private Adb adb = new Adb();
 	
 	public ExecutionEngine(StaticApp testApp) {
 		this.testApp = testApp;
@@ -19,19 +20,22 @@ public class ExecutionEngine {
 	
 	public StaticApp buildPathSummaries() {
 		
+		adb.uninstallApp(testApp.getPackageName());
+		adb.installApp(testApp.getSignedAppPath());
+		
 		Execution cE = new Execution(testApp);
 		cE.printOutPS = false;
 		///////////////
 		UIModelGenerator builder = new UIModelGenerator(testApp);
 		builder.buildOrRead(false);
 		
-		UIModelGraph model = builder.getUIModel();
-		model.enableGUI();
+		//UIModelGraph model = builder.getUIModel();
+		//model.enableGUI();
 		
-/*		System.out.println("getEventDeposit");
+		System.out.println("getEventDeposit");
 		for(Event e: builder.getEventDeposit()){
-			System.out.println(e.getEventType());
-		}*/
+			System.out.println(e);
+		}
 
 		System.out.println("getEventMethodMap");
 		for(Entry<String, List<Event>>  entry : builder.getEventMethodMap().entrySet() ){
